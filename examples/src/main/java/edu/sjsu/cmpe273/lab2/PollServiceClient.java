883ecd1f1f066d4f2aa369003f1ee66321748468
Dhwani Shah <dhwani.shah0912@gmail.com>
2015-03-18 15:08:56 -0700

    Commit1
    Commit1

---
+++ examples/src/main/java/edu/sjsu/cmpe273/lab2/PollServiceClient.java
@@ -0,0 +1,92 @@
+/*
+ * Copyright 2015, Google Inc. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ *
+ *    * Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ *    * Redistributions in binary form must reproduce the above
+ * copyright notice, this list of conditions and the following disclaimer
+ * in the documentation and/or other materials provided with the
+ * distribution.
+ *
+ *    * Neither the name of Google Inc. nor the names of its
+ * contributors may be used to endorse or promote products derived from
+ * this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package edu.sjsu.cmpe273.lab2;
+
+import io.grpc.ChannelImpl;
+import io.grpc.transport.netty.NegotiationType;
+import io.grpc.transport.netty.NettyChannelBuilder;
+
+import java.util.concurrent.TimeUnit;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ * A simple client that requests a greeting from the {@link HelloWorldServer}.
+ */
+public class PollServiceClient {
+  private static final Logger logger = Logger.getLogger(PollServiceClient.class.getName());
+
+  private final ChannelImpl channel;
+  private final PollServiceGrpc.PollServiceBlockingStub blockingStub;
+
+  public PollServiceClient(String host, int port) {
+    channel =
+        NettyChannelBuilder.forAddress(host, port).negotiationType(NegotiationType.PLAINTEXT)
+            .build();
+    blockingStub = PollServiceGrpc.newBlockingStub(channel);
+  }
+
+  public void shutdown() throws InterruptedException {
+    channel.shutdown().awaitTerminated(5, TimeUnit.SECONDS);
+  }
+
+  public void createPoll(String name) {
+    try {
+      logger.info("Will try to greet " + name + " ...");
+      PollRequest request = PollRequest.newBuilder().setModeratorId(name).setQuestion(name).build();
+      //request.setModeratorId(name).build();
+      //request.setQuestion(name).build();
+      //request.setStartedAt(name).build();
+      //request.setExpiredAt(name).build();
+      //request.setChoice(new String[]{name,name}).build();
+      PollResponse response = blockingStub.createPoll(request);
+//      logger.info(“Greeting”);
+    } catch (RuntimeException e) {
+      logger.log(Level.WARNING, "RPC failed", e);
+      return;
+    }
+  }
+
+  public static void main(String[] args) throws Exception {
+    PollServiceClient client = new PollServiceClient("localhost", 50051);
+    try {
+      /* Access a service running on the local machine on port 50051 */
+      String user = "world";
+      if (args.length > 0) {
+        user = args[0]; /* Use the arg as the name to greet if provided */
+      }
+      client.createPoll(user);
+    } finally {
+      client.shutdown();
+    }
+  }
+}
